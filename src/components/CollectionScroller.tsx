be7bea212b4fd516b0fb327d33d9289c3fa623dd
Rahuljaluthria <rahuljaluthria321@gmail.com>
2026-08-09 11:29:46 +0530

    changed jewellery cover page

--- src/components/CollectionScroller.tsx
+++ src/components/CollectionScroller.tsx
@@ -6,7 +6,7 @@ import { usePageTransition } from '../features/PageTransitionContext';
 import clothingDefault from '../assets/static/image1.jpg';
 import menClothing from '../assets/static/zoom.jpg';
 import womenClothing from '../assets/static/front.jpeg';
-import jewelleryCover from '../assets/static/jewelHome.webp';
+import jewelleryCover from '../assets/jewellery cover page .jpeg';
 import jewelleryMen from '../assets/men jewellery.png';
 import jewelleryWomen from '../assets/women jewellery.jpeg';
 
